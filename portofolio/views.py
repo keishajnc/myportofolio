62c3c21fde2a5ad733c346e4248737cc5d8d512b
keishajnc <keishanapitupulu@gmail.com>
2026-09-13 21:33:20 +0700

    refactor: remove old skills view

--- portofolio/views.py
+++ portofolio/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
-from main.models import Skill
+
 
 def landing_page(request):
     return render(request, "index.html")
-
-def skill_page(request):
-    skills = Skill.objects.all()
-
-    return render(request, "skill.html", {
-        "skills": skills
-    })

1a168adad0d8152ba7adbcfa937481585f136df3
keishajnc <keishanapitupulu@gmail.com>
2026-09-21 08:03:16 +0700

    feat: add experience update route

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,8 @@ from main.views import (
     show_main, show_experience, show_skill,
     create_experience, create_skill,
     get_experiences_json, get_skills_json,
-    delete_experience, delete_skill
+    delete_experience, delete_skill,
+    update_experience
 )
 
 app_name = "main"
@@ -13,6 +14,7 @@ urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
     path("experience/add/", create_experience, name="create_experience"),
+    path("experience/<uuid:id>/edit/", update_experience, name="update_experience"),
     path("api/experience/", get_experiences_json, name="get_experiences_json"),
     path("experience/<uuid:id>/delete/", delete_experience, name="delete_experience"),
     path("skills/", show_skill, name="show_skill"),
